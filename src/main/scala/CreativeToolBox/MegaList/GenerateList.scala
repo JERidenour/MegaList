56c7b4802d707a50ad19ab5504c30937f175e986
Master Control <fireflightning@gmail.com>
2017-07-29 23:56:25 +0200

    reformated test string to look like the princeton example

--- src/main/scala/CreativeToolBox/MegaList/GenerateList.scala
+++ src/main/scala/CreativeToolBox/MegaList/GenerateList.scala
@@ -3,7 +3,19 @@ package CreativeToolBox.Megalist
 object GenerateList{
     def main(args: Array[String]) {
 
-        val testString = "This is a string for testing not a string to use"
+        val testString = "bbb bbb " +
+            "bbb bba " +
+            "bbb bba " + 
+            "bba bab " +
+            "bab aba " + 
+            "bab abb " + 
+            "bab abb " + 
+            "bab abb " + 
+            "abb bbb " + 
+            "abb bbb " + 
+            "abb bba " +
+            "aba bab" 
+             
         var chain = MarkovChain(testString)
         println(chain.toString)
 
