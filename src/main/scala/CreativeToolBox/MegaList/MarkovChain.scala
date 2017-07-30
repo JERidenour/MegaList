dc395d32e5c02e6292bdc9c765851260f0bcde8e
Master Control <fireflightning@gmail.com>
2017-07-30 13:37:08 +0200

    if possibleStates has length 0, revert to initial state

--- src/main/scala/CreativeToolBox/MegaList/MarkovChain.scala
+++ src/main/scala/CreativeToolBox/MegaList/MarkovChain.scala
@@ -19,26 +19,30 @@ class MarkovChain {
 
         // get the set of states which start with sChar
         var possibleStates = this.states.filter( _(0) == sChar )
-        // TODO: handle cases where filter argument returns false
+        // TODO: handle cases where filter argument returns false (invalid sChar)
 
         // draw at random from the resulting population, set this as current state
         val rnd = new scala.util.Random
         var index = rnd.nextInt( possibleStates.length )
-        this.currentState = possibleStates.apply(index) 
+        val initialState = possibleStates.apply(index) 
+        this.currentState = initialState
 
-        // repeat until the desired length is achived        
+        // repeat either until the desired length is achived        
+        // or until the model runs out of states
         var sim = new ArrayBuffer[String]
         while ( sim.length < (len + 1)  ) {
             sim += this.currentState
             possibleStates = this.states
                 .filter(  _(0) == this.currentState( this.currentState.length-1 ) )
-            index = rnd.nextInt( possibleStates.length )
-            //TODO: handle case where possibleStates has length = 0
 
-            println( s"index = $index" )
-            this.currentState = possibleStates.apply(index)
-            // TODO: make sure the index is always valid
-            sim += this.currentState
+            // if there are no possible states, revert to the initial state 
+            // otherwise update
+            if( possibleStates.length==0 ){
+                this.currentState = initialState
+            } else {
+                index = rnd.nextInt( possibleStates.length )
+                this.currentState = possibleStates.apply(index)
+            }
         }
         sim.mkString
     } 
