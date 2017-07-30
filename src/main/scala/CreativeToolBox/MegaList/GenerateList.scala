e1adb7f941f47e650607c3381a94e2b61f69712b
Master Control <fireflightning@gmail.com>
2017-07-30 13:47:11 +0200

    read the input string from a text file

--- src/main/scala/CreativeToolBox/MegaList/GenerateList.scala
+++ src/main/scala/CreativeToolBox/MegaList/GenerateList.scala
@@ -1,11 +1,16 @@
 package CreativeToolBox.Megalist
 
+import scala.io.Source
+
 object GenerateList{
     def main(args: Array[String]) {
 
-        //val testString = "bbbabbabbbbaba"             
-        val testString = "this is a test string with actual words"             
-        var chain = MarkovChain(testString, 4)
-        println(chain.simulate('t', 5))
+        val filename = "src/main/resources/training_data.txt"
+        val bufferedSource = Source.fromFile(filename)
+        val fileContents = bufferedSource.getLines.mkString
+        bufferedSource.close
+
+        var chain = MarkovChain(fileContents, 4)
+        println(chain.simulate(' ', 5))
     }
 }
