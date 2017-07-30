a7687929993eca3a7e457a186bc7ec81c3312535
Master Control <fireflightning@gmail.com>
2017-07-30 22:21:33 +0200

    write output to file

--- src/main/scala/CreativeToolBox/MegaList/GenerateList.scala
+++ src/main/scala/CreativeToolBox/MegaList/GenerateList.scala
@@ -1,29 +1,34 @@
 package CreativeToolBox.Megalist
 
 import scala.io.Source
+import java.nio.file.{Paths, Files}
+import java.nio.charset.StandardCharsets
 
 object GenerateList{
     def main(args: Array[String]) {
 
-        val filename = "src/main/resources/dylan_thomas.txt"
-        val bufferedSource = Source.fromFile(filename)
+        val inputFile = "src/main/resources/Full_Collection_of_Dylan_Thomas.txt"
+        val bufferedSource = Source.fromFile(inputFile)
         val fileContents = bufferedSource.getLines.mkString(" ")
         bufferedSource.close
 
         /*
         * Note: order 3 adds two words at a time, order 4 three words, etc
         */
-        var chain = MarkovChain(fileContents, 2)
-        val maxLength = 4
-        for ( i <- 1 to 10 ) {
+        var chain = MarkovChain(fileContents, 3)
+        val maxLength = 3
+//        for ( i <- 1 to 15 ) {
+//
+//            var randInt = scala.util.Random.nextInt( (maxLength) ) 
+//	    println(randInt + 1 )
+//            println(chain.simulate(randInt))
+//
+//        }
 
-            var randInt = scala.util.Random.nextInt( (maxLength-1) ) 
-            println(chain.simulate(randInt))
+	//println(chain.simulate(100))
 
-        }
-
-//            var randChar = scala.util.Random.alphanumeric.filter(_.isLetter).head
-//            println(chain.simulate(100))
+	val outputFile = "src/main/resources/Output.txt"
+	Files.write(Paths.get(outputFile), chain.simulate(100).getBytes(StandardCharsets.UTF_8))
 
     }
 }
