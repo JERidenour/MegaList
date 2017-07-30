526a5113e773bf2c65ba487c2e0a011423ef3ad7
Master Control <fireflightning@gmail.com>
2017-07-30 13:05:04 +0200

    commented

--- src/main/scala/CreativeToolBox/MegaList/MarkovChain.scala
+++ src/main/scala/CreativeToolBox/MegaList/MarkovChain.scala
@@ -11,15 +11,24 @@ class MarkovChain {
     
     def currentState_= (in: String):Unit = _currentState = in
 
-    def simulate(sChar: Char, length: Int): String = {
+    /*
+    * Simulate a path of length [[len]] through the markov chain
+    * with initial state chosen randomly from states starting with [[sChar]].
+    */
+    def simulate(sChar: Char, len: Int): String = {
+
+        // get the set of states which start with sChar
         var possibleStates = this.states.filter( _(0) == sChar )
         // TODO: handle cases where filter argument returns false
+
+        // draw at random from the resulting population, set this as current state
         val rnd = new scala.util.Random
         var index = rnd.nextInt( possibleStates.length )
         this.currentState = possibleStates.apply(index) 
-        
+
+        // repeat until the desired length is achived        
         var sim = new ArrayBuffer[String]
-        while ( sim.length < (length + 1)  ) {
+        while ( sim.length < (len + 1)  ) {
             sim += this.currentState
             possibleStates = this.states
                 .filter(  _(0) == this.currentState( this.currentState.length-1 ) )
