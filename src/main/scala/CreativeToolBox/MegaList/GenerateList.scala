d4164b9f16e52e592e121bbd2246a069c1262ead
Master Control <fireflightning@gmail.com>
2017-08-10 09:39:07 +0200

    for each state, get companion states

--- src/main/scala/CreativeToolBox/MegaList/GenerateList.scala
+++ src/main/scala/CreativeToolBox/MegaList/GenerateList.scala
@@ -8,7 +8,7 @@ import scala.util.Random
 object GenerateList{
     def main(args: Array[String]) {
 
-        val inputFile = "src/main/resources/The_quran.txt"
+        val inputFile = "src/main/resources/Dylan_Thomas.txt"
         val bufferedSource = Source.fromFile(inputFile)
         val fileContents = bufferedSource.getLines.mkString(" ")
         bufferedSource.close
@@ -17,21 +17,34 @@ object GenerateList{
         * Note: the order parameter is basically how many words there are per node
         */
         var chain = MarkovChain(fileContents, 2)
-        val maxLength = 3
-	var outString = ""
-        for ( i <- 1 to 300 ) {
+        //val maxLength = 3
+	//var outString = ""
+        //for ( i <- 1 to 300 ) {
 
-            var randInt = Random.nextInt( (maxLength) ) 
-            outString += "\n " + chain.simulate(randInt)
+        //    var randInt = Random.nextInt( (maxLength) ) 
+        //    outString += "\n " + chain.simulate(randInt)
+
+        //}
+
+	////val outString = chain.simulate(100)
+	////println(outString))
+
+	//val outputFile = "src/main/resources/Output.txt"
+	//Files.write(Paths.get(outputFile),outString 
+	//    .getBytes(StandardCharsets.UTF_8))
+
+        for(s <- chain.states){
+
+            //for each state, get companion states
+            var keyWord = s.split(" ").last
+            var compStates = chain.states.filter( (a:String) => a.split(" ").head == keyWord)
+            
+            println(s) 
+            println(compStates) 
+            //for each companion state, get prob and write edge
+            
+            
 
         }
-
-	//val outString = chain.simulate(100)
-	//println(outString))
-
-	val outputFile = "src/main/resources/Output.txt"
-	Files.write(Paths.get(outputFile),outString 
-	    .getBytes(StandardCharsets.UTF_8))
-
     }
 }
