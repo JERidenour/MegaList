7f7ab1e744899f7d3471a4c786c12f6bcc467a11
Master Control <fireflightning@gmail.com>
2017-08-10 11:12:14 +0200

    added println statements

--- build.sbt
+++ build.sbt
@@ -4,7 +4,7 @@ version := "1.0"
 
 scalaVersion := "2.12.2"
 
-lazy val root = Project("root", file(".")) dependsOn(scalaGraphvizProject)
 lazy val scalaGraphvizProject = RootProject(uri("git://github.com/JERidenour/ScalaGraph.git"))
+lazy val root = Project("root", file(".")) dependsOn(scalaGraphvizProject)
 
 //libraryDependencies += "com.github.whatever" %% "etc" % "etc"
--- src/main/scala/CreativeToolBox/MegaList/GenerateList.scala
+++ src/main/scala/CreativeToolBox/MegaList/GenerateList.scala
@@ -3,13 +3,14 @@ package CreativeToolBox.Megalist
 import scala.io.Source
 import java.nio.file.{Paths, Files}
 import java.nio.charset.StandardCharsets
+import sys.process._
 import scala.util.Random
 import net.graphviz._
 
 object GenerateList{
     def main(args: Array[String]) {
 
-        val inputFile = "src/main/resources/Dylan_Thomas.txt"
+        val inputFile = "src/main/resources/dylan_thomas.txt"
         val bufferedSource = Source.fromFile(inputFile)
         val fileContents = bufferedSource.getLines.mkString(" ")
         bufferedSource.close
@@ -17,6 +18,7 @@ object GenerateList{
         /*
         * Note: the order parameter is basically how many words there are per node
         */
+        println("initializeing Markov chain")
         var chain = MarkovChain(fileContents, 2)
         //val maxLength = 3
 	//var outString = ""
@@ -34,20 +36,33 @@ object GenerateList{
 	//Files.write(Paths.get(outputFile),outString 
 	//    .getBytes(StandardCharsets.UTF_8))
 
+        println("initializeing Dot object")
         var dot = new Dot()
         chain.states.foreach { dot.nodeBuffer += Node.apply(_)  }
 
-        val N = chain.states.length
         for(s <- chain.states){
 
             //for each state, get companion states
             var keyWord = s.split(" ").last
             var compStates = chain.states.filter( (a:String) => a.split(" ").head == keyWord)
+            var N = compStates.length.toDouble
+    
+            for(cs <- compStates){
             
-            //for each companion state, get prob and write edge
-            var prob = compStates.length / N
-                        
-
+                //for each companion state, get prob and write edge
+                var prob = (compStates.filter( (a:String)  => a  == cs ).length + 1) / N
+                dot.edgeBuffer += Edge.apply(s, cs)
+            }
         }
+
+        println("writing Dot to file")
+        Files.write(Paths.get("src/main/resources/mcGraph.dot"), 
+            dot.toString.getBytes(StandardCharsets.UTF_8))
+
+        println("executing system call to graphviz")
+        // system call to graphviz
+        "neato src/main/resources/mcGraph.dot -Tpng -osrc/main/resources/mcGraph.png " !
+
+
     }
 }
